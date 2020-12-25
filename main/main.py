e6beb47f30a90ecba0456cf2f90d0395d6619fe1
pochih-kuo <75714572+pochih-kuo@users.noreply.github.com>
2020-12-25 15:01:09 +0800

    adjust game1 in main.py

--- main/main.py
+++ main/main.py
@@ -882,7 +882,31 @@ def game1_start():
     PLAYER_SPEED_X = 5
     space = pygame.image.load('G1-background.png')
     # pygame.init()
+    health = pygame.image.load("G1-health.png")
+    money = pygame.image.load("G1-money.png")
+    friend = pygame.image.load("G1-friend.png")
+#--------------------------------------------------------------------------------------------------------
+    window_surface = pygame.display.set_mode((600, 600))
+    WINDOW_WIDTH=600
+    WINDOW_HEIGHT=600
 
+    class Line(pygame.sprite.Sprite):
+        def __init__(self, width, height, x_position, y_position, window_width, window_height, path):
+            super().__init__()
+            # 載入圖片
+            self.raw_image = pygame.image.load(path).convert_alpha()
+            # 縮小圖片
+            self.image = pygame.transform.scale(self.raw_image, (width, height))
+            #  回傳位置
+            self.rect = self.image.get_rect()
+            #  定位
+            self.rect.topleft = (x_position, y_position)
+            self.width = width
+            self.height = height
+            self.window_width = window_width
+            self.window_height = window_height
+
+#---------------------------------------------------------------------------------------------------------
     class Student:
         def __init__(self):
             # self.game = game
@@ -949,14 +973,14 @@ def game1_start():
         return False
 
     def draw_text(text, size, color, x, y):
-        font = pygame.font.SysFont('arial', size)
+        font = pygame.font.SysFont('SourceHanSansTC-Bold.otf', size)
         text_surface = font.render(text, True, color)
         text_rect = text_surface.get_rect()
         text_rect.center = (x, y)
         gameDisplay.blit(text_surface, text_rect)
 
     gameDisplay = pygame.display.set_mode((600, 600))
-    # pygame.display.set_caption('Dodge people on Palm Avenue!')
+    #pygame.display.set_caption('Dodge people on Palm Avenue!')
     clock = pygame.time.Clock()
     last_spawn_stone = pygame.time.get_ticks()    # 獲取時間(毫秒)
     while True:
@@ -964,16 +988,28 @@ def game1_start():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
-        gameDisplay.fill((255, 255, 255))
-        draw_text("Dodge people on Palm Avenue!",40, (0, 0, 0), 300, 200)    # draw_text
-        draw_text("Operations guide : Use ↑ ↓ ← → to dodge people",20, (0, 0, 0), 300, 250)
-        draw_text("in 30 seconds",20, (0, 0, 0), 300, 300)
+        gameDisplay.fill((0, 0, 0))
+#-----------------------------------------------------------------------------------------------
+        RESUME = Line(157, 34, 350,400, 600, 600, 'G2-resume.png')
+            # cursor
+        if RESUME.rect.topleft[0] < pygame.mouse.get_pos()[0] < RESUME.rect.topleft[0] + RESUME.width \
+            and RESUME.rect.topleft[1] < pygame.mouse.get_pos()[1] < RESUME.rect.topleft[1] + RESUME.height:
+            pygame.mouse.set_cursor(*pygame.cursors.diamond)
+        else:
+            pygame.mouse.set_cursor(*pygame.cursors.tri_left)       
+
+        background_raw = pygame.image.load('G2-intro1.png')
+        background = pygame.transform.scale(background_raw, (600, 600))
+        background.convert()
+        window_surface.blit(background, (0, 0))
+        window_surface.blit(RESUME.image, RESUME.rect)
+        
         # button("Start", 250, 350, 100, 50,(200, 0, 0), (255, 0, 0))    # button    # new
 
         mouse = pygame.mouse.get_pos()
         click = pygame.mouse.get_pressed()
-        if (mouse[0] > 250 and mouse[0] < 250+100) and (mouse[1] > 350 and mouse[1] < 350+50):
-            pygame.draw.rect(gameDisplay, (255, 0, 0), (250, 350, 100, 50))
+        if RESUME.rect.topleft[0] < pygame.mouse.get_pos()[0] < RESUME.rect.topleft[0] + RESUME.width \
+            and RESUME.rect.topleft[1] < pygame.mouse.get_pos()[1] < RESUME.rect.topleft[1] + RESUME.height:
             if click[0] == 1:
                 
                 #new()
@@ -982,30 +1018,33 @@ def game1_start():
                 student = Student()
 
                 while True:
-                    if pygame.time.get_ticks() > 30000:    # 10秒
-                        draw_text("You AG!", 50, (255, 255, 255), 300, 300)
+                    if pygame.time.get_ticks() > 25000:    #25秒
+                        draw_text("You AG!", 80, (255, 255, 255), 300, 300)
                         final = 0
                         pygame.display.update()
-                        time.sleep(2)
-                        # draw()
+                        time.sleep(1)
                         gameDisplay.fill((255, 255, 255))
                         gameDisplay.blit(space, (0,0,600,600)) 
                         pygame.display.update()
                         time.sleep(1)
-                        draw_text("academic_performance = 60", 20, (255, 255, 255), 300, 300)
-                        draw_text("romantic_relationship = 60", 20, (255, 255, 255), 300, 275)
-                        draw_text("interpersonal_relationship = 40", 20, (255, 255, 255), 300, 250)
-                        draw_text("health = 40", 20, (255, 255, 255), 300, 225)
-                        draw_text("wealth = 30", 20, (255, 255, 255), 300, 200)
+                        gameDisplay.blit(health, (60,60,50,70))
+                        gameDisplay.blit(friend, (60,140,50,140))
+                        draw_text("+20", 60, (0,0,0),160,100)
+                        draw_text("+20", 60, (0,0,0),160,170)
+                        #draw_text("academic_performance = 60", 20, (255, 255, 255), 300, 300)
+                        #draw_text("romantic_relationship = 60", 20, (255, 255, 255), 300, 275)
+                        #draw_text("interpersonal_relationship = 60", 20, (255, 255, 255), 300, 250)
+                        #draw_text("health = 60", 20, (255, 255, 255), 300, 225)
+                        #draw_text("wealth = 60", 20, (255, 255, 255), 300, 200)
                         pygame.display.update()
                         ######印出減分
-                        time.sleep(2)
-                        romantic_relationship = 10
+                        time.sleep(5)
+                        romantic_relationship = 0
                         money = 0
-                        health = 0
+                        health_value = +20
                         academic_performance = 0
-                        interpersonal_relationship = 0
-                        return romantic_relationship, money, health, academic_performance,interpersonal_relationship
+                        interpersonal_relationship = +20
+                        return romantic_relationship, money, health_value, academic_performance,interpersonal_relationship
                         
                     for event in pygame.event.get():
                         if event.type == pygame.QUIT:
@@ -1027,32 +1066,39 @@ def game1_start():
                     for stone in stones:    # 每顆石頭檢查碰撞
                         if collision_check(student, stone):    # collision_check
                             final = 1
-                            draw_text("Lose!", 50, (255, 255, 255), 300, 300)
+                            draw_text("Lose!", 80, (255, 255, 255), 300, 300)
                             pygame.display.update()
-                            time.sleep(2)
-                            # draw()
+                            time.sleep(1)
                             gameDisplay.fill((255, 255, 255))
                             gameDisplay.blit(space, (0,0,600,600)) 
                             pygame.display.update()
                             time.sleep(1)
-                            draw_text("academic_performance = 60", 20, (255, 255, 255), 300, 300)
-                            draw_text("romantic_relationship = 60", 20, (255, 255, 255), 300, 275)
-                            draw_text("interpersonal_relationship = 40", 20, (255, 255, 255), 300, 250)
-                            draw_text("health = 40", 20, (255, 255, 255), 300, 225)
-                            draw_text("wealth = 30", 20, (255, 255, 255), 300, 200)
+                            gameDisplay.blit(health, (60,60,50,70))
+                            gameDisplay.blit(money, (60,140,50,140))
+                            gameDisplay.blit(friend, (60,210,50,210))
+                            draw_text("-30", 60, (0,0,0),160,100)
+                            draw_text("-30", 60, (0,0,0),160,170)
+                            draw_text("-20", 60, (0,0,0),160,240)
+                            #draw_text("academic_performance = 60", 20, (255, 255, 255), 300, 300)
+                            #draw_text("romantic_relationship = 60", 20, (255, 255, 255), 300, 275)
+                            #draw_text("interpersonal_relationship = 40", 20, (255, 255, 255), 300, 250)
+                            #draw_text("health = 30", 20, (255, 255, 255), 300, 225)
+                            #draw_text("wealth = 30", 20, (255, 255, 255), 300, 200)
+                            
                             pygame.display.update()
-                            time.sleep(2)
+                            time.sleep(5)
                             romantic_relationship = 0
                             money = -30
-                            health = -30
+                            health_value = -30
                             academic_performance = 0
                             interpersonal_relationship = -20
-                            return romantic_relationship, money, health, academic_performance,interpersonal_relationship
+
+                            return romantic_relationship, money, health_value, academic_performance,interpersonal_relationship
 
                     
                     #draw()
                     gameDisplay.fill((255, 255, 255))
-                    gameDisplay.blit(space, (0,0,600,600))    # 背景尺寸要改
+                    gameDisplay.blit(space, (0,0,600,600))
                     student.draw()
                     for stone in stones:
                         stone.draw()
@@ -1060,8 +1106,7 @@ def game1_start():
                     clock.tick(60)
 
         else:
-            pygame.draw.rect(gameDisplay, (200, 0, 0), (250, 350, 100, 50))
-        draw_text('Start', 25, (255, 255, 255), 250+(100/2), 350+(50/2))
+            pass
 
         pygame.display.update()
         clock.tick(30)
